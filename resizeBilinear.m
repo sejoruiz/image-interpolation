188dd91777246d763a7a1c9e42c0d27bcfd1a1d1
Sergio <sejoruiz@gmail.com>
2014-03-07 14:31:29 +0100

    Adding GNU GPLv3 license

--- resizeBilinear.m
+++ resizeBilinear.m
@@ -1,3 +1,20 @@
+% Author: Sergio Ruiz
+% Date: 07/03/2014
+%   This file is part of imageInterpolations.
+
+%    imageInterpolations is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    imageInterpolations is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
+%    You should have received a copy of the GNU General Public License
+%    along with imageInterpolations.  If not, see <http://www.gnu.org/licenses/>.
+
 function [ scaled_image ] = resizeBilinear( img, scale, gray )
 %RESIZEBILINEAR Resizes a picture using bilinear interpolation
 %   The function will take two args:
